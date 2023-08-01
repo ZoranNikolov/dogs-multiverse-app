a304916d8e32d186ed6614f8e4649280f701f752
Zoran Nikolov <zoran902@gmail.com>
2023-08-01 16:51:05 +0300

    added bootstrap and pages

---
+++ src/app/pages/terms-and-coditions/terms-and-coditions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terms-and-coditions',
+  templateUrl: './terms-and-coditions.component.html',
+  styleUrls: ['./terms-and-coditions.component.css']
+})
+export class TermsAndCoditionsComponent {
+
+}
