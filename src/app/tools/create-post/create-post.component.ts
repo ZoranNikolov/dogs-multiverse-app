ae4aa87f3da01c454c47d64ebf49cd1c39424b43
Zoran Nikolov <zoran902@gmail.com>
2023-08-11 16:25:51 +0300

    added preview functionality

--- src/app/tools/create-post/create-post.component.ts
+++ src/app/tools/create-post/create-post.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-create-post',
-  templateUrl: './create-post.component.html',
-  styleUrls: ['./create-post.component.css']
+	selector: 'app-create-post',
+	templateUrl: './create-post.component.html',
+	styleUrls: ['./create-post.component.css'],
 })
 export class CreatePostComponent {
+	selectedImageFile!: File;
 
+	constructor() {}
+
+	onPhotoSelected(photoSelector: HTMLInputElement) {
+		const files = photoSelector.files;
+		if (!files || !files.length) {
+			return;
+		}
+
+		this.selectedImageFile = files[0];
+		let fileReader = new FileReader();
+		fileReader.readAsDataURL(this.selectedImageFile);
+
+		fileReader.addEventListener('loadend', (ev) => {
+			let readableString = fileReader.result as string | undefined;
+			let postPreviewImage = <HTMLImageElement>(
+				document.getElementById('post-preview-image')
+			);
+
+			if (postPreviewImage && readableString) {
+				postPreviewImage.src = readableString;
+			}
+		});
+	}
 }
